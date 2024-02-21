0ac9722dfc3f8c1b0dee31d947990e1db4c31c32
EduardRK <Eduard.ivanushkin.100@gmail.com>
2024-02-21 22:47:40 +0300

    First task without tests

---
+++ scrapper/src/main/java/edu/java/scrapper_body/clients/unsupported_client/UnsupportedClient.java
@@ -0,0 +1,17 @@
+package edu.java.scrapper_body.clients.unsupported_client;
+
+import edu.java.scrapper_body.clients.clients_body.Client;
+import edu.java.scrapper_body.clients.clients_body.Response;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class UnsupportedClient implements Client {
+    public UnsupportedClient() {
+    }
+
+    @Override
+    public List<Response> newUpdates(URI uri) {
+        return new ArrayList<>();
+    }
+}
---
+++ scrapper/src/test/java/edu/java/configuration/ApplicationConfigTest.java
@@ -0,0 +1,16 @@
+package edu.java.configuration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.java.scrapper_body.clients.github_client.GitHubCommitUpdateClient;
+import edu.java.scrapper_body.clients.stackoverflow_client.StackOverflowQuestionClient;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.Test;
+
+class ApplicationConfigTest {
+
+    @Test
+    void scheduler() throws URISyntaxException, JsonProcessingException {
+
+    }
+}
