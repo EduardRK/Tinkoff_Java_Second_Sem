57466e53c87ca4655964facee259183553057534
EduardRK <Eduard.ivanushkin.100@gmail.com>
2024-03-15 23:14:11 +0300

    Fix checkstyle

--- bot/src/main/java/edu/java/bot/controller/ExceptionController.java
+++ bot/src/main/java/edu/java/bot/controller/ExceptionController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
