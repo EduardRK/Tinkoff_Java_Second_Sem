c81075c3c33bf8424bcb69ac1a0bfd7bf8fe37d1
EduardRK <Eduard.ivanushkin.100@gmail.com>
2024-04-14 10:07:34 +0300

    Add new metric Small fixes

--- scrapper/src/test/java/edu/java/configuration/ClientChainConfigTest.java
+++ scrapper/src/test/java/edu/java/configuration/ClientChainConfigTest.java
@@ -1,9 +1,9 @@
 package edu.java.configuration;
 
-import java.time.Duration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.util.retry.Retry;
+import java.time.Duration;
 
 class ClientChainConfigTest {
     Retry retry = Retry.fixedDelay(1, Duration.ofSeconds(10));
